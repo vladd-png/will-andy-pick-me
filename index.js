0b09105884f136f35e2025060ac1b63b0547411e
Virginia Ladd <virginialadd2@gmail.com>
2019-10-13 13:12:51 -0600

    Almost finish iteration two

--- index.js
+++ index.js
@@ -6,34 +6,25 @@ var ball = document.querySelector(".eightBall");
 
 btn.addEventListener("click", function () {
   var inputText = document.getElementById("input").value;
+  answer.classList.remove("question");
+  answer.classList.add("submitStyle");
   answer.innerText = inputText;
   ball.style.display = "none";
-  answerPrint.classList.remove("question");
 });
 
 //generate random messages
-
-// function toggleDisplay () {
-//   var x = document.querySelector(".submitForm");
-//   if (x.style.display = "none") {
-
 var answer2 = document.querySelector("p");
 var btn = document.getElementById("button");
 var answerPrint = document.querySelector(".answer");
-var messages = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely."]
+var messages = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again."]
 
-
+// display message on click
 btn.addEventListener("click", function () {
   var randomAnswer = Math.floor(Math.random() * messages.length);
   answer2 = messages[randomAnswer];
   answerPrint.classList.remove("answer");
+  answerPrint.classList.add("submitStyle")
+  answerPrint.innerText = answer2;
 
-
-  console.log(answerPrint);
+  console.log(answer2);
 });
-
-// } else {
-//   x.style.display = "none";
-// }
-//
-// }
